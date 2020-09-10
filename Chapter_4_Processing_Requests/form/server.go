8eae1ceba4a9f92482233e82ea4391517cdf3af5
xuziyao <coolcoolxu@gmail.com>
2020-09-10 15:21:43 +0800

    add some new Form operations

--- Chapter_4_Processing_Requests/form/server.go
+++ Chapter_4_Processing_Requests/form/server.go
@@ -7,7 +7,7 @@ import (
 
 func process(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
-	fmt.Fprintln(w, r.PostForm)
+	fmt.Fprintln(w, r.Form,r.PostForm,r.Form["hello"],r.FormValue("hello"))
 }
 
 func main() {
