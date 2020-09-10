ed6488fae6944244fcd50b8edfab212e704fa6a1
xuziyao <coolcoolxu@gmail.com>
2020-09-10 19:56:30 +0800

    limit file size

--- Chapter_4_Processing_Requests/fileupload/server.go
+++ Chapter_4_Processing_Requests/fileupload/server.go
@@ -7,9 +7,29 @@ import (
 )
 
 func process(w http.ResponseWriter, r *http.Request) {
-	r.ParseMultipartForm(1024)
-	fileHeader := r.MultipartForm.File["uploaded"][0]
-	file, err := fileHeader.Open()
+	r.Body = http.MaxBytesReader(w, r.Body, 30)
+	// var p = make([]byte, 6*1024*1024)
+	// n, err := r.Body.Read(p)
+	// if err != nil {
+	// 	fmt.Println(n, err)
+	// }
+	// if err == nil {
+	// 	fmt.Println(n, err)
+	// }
+	r.ParseMultipartForm(6*1024*1024)
+	// fileHeader := r.MultipartForm.File["uploaded"][0]
+	// if fileHeader == nil {
+	// 	fmt.Println("can't find file!")
+	// 	fmt.Fprintln(w, "can't find file!")
+	// 	return
+	// }
+	// file, err := fileHeader.Open()
+	file, _ , err := r.FormFile("uploaded")
+	if file == nil {
+		fmt.Println("can't find file!")
+		fmt.Fprintln(w, "can't find file!")
+		return
+	}
 	if err == nil {
 		data, err := ioutil.ReadAll(file)
 		if err == nil {
